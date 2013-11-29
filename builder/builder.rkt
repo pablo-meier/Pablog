d8fdce86334f6783f4a5be775bc37bf5dec02073
Paul Meier <pablo.a.meier@gmail.com>
2013-11-29 11:43:53 -0500

    Unhelpful and juvenile comments in build script.
    I can do better.

--- builder/builder.rkt
+++ builder/builder.rkt
@@ -49,14 +49,12 @@
   (for ([curr (directory-list *static-files* #:build? #t)])
     (ez-move curr *build-dir*)))
 
-;; Cool, Racket can only copy the *contents* of a directory into a fresh directory; you need to
-;; make one with the same name yourself. Also, it fails if one of the directories you want to copy into already
-;; exists. 
-;; nbd
+;; Racket's file and directory copy routines have some failure modes that don't make the kind of copy I'd like
+;; to do (copy recursively with directories, and copy over if they exist) particularly easy. This little
+;; wrapper just does what I want it to, no questions asked.
 (define (ez-move path dst)
-  ;; This next bit is a bit stupid... I want the last value of the path. I can get this with 'split-path',
-  ;; but it returns 3 values using the archaic "values" expression, meaning it's not in a list. To access
-  ;; that middle value, I have to use "call-with-values".
+  ;; I want the last value of the path. I can get this with 'split-path', but it returns 3 values using "values",
+  ;; meaning it's not in a list. To access that middle value, I have to use "call-with-values".
   (define path-suffix (call-with-values (λ () (split-path path)) (λ (a b c) (path->string b))))
   (define new-dst (string-append dst "/" path-suffix))
   (cond
