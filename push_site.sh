a33f57e4db5f789ede4297cfd38a251454fe80c6
Pablo Meier <pablo.a.meier@gmail.com>
2017-12-20 18:11:21 -0500

    new push script

---
+++ push_site.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copy this file with the correct value set as AMAZON_DISTRIBUTION_ID to push to S3
+#
+
+if [ -z "$AMAZON_DISTRIBUTION_ID" ];
+then
+    echo 'Must set $AMAZON_DISTRIBUTION_ID'
+    exit 1
+fi
+
+aws s3 sync --acl public-read --sse --delete ./build/ s3://www.morepablo.com
+aws configure set preview.cloudfront true
+aws cloudfront create-invalidation --distribution-id $AMAZON_DISTRIBUTION_ID --paths '/*'
